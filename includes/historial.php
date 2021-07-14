d445b7684a78c123fdc992ca8fe310787ca23ebc
XekeL121 <oscarmahiques@gmail.com>
2021-07-14 21:43:23 +0200

    Retoques responsive

--- includes/historial.php
+++ includes/historial.php
@@ -1,6 +1,6 @@
 <div class="ofertas p-2 px-5 row text-center justify-content-center overflow-scroll azulFondo">
                     <!-- Aquí comienza el portfolio -->
-                    <div class="tarjetas px-2 my-3 col col-lg-9 col-xl-8 col-md-8 col-8 text-center azul1 rounded">
+                    <div class="tarjetas px-2 my-3 col col-lg-9 col-xl-8 col-md-8 col-8 text-center  rounded">
                         <h3 class="mt-3 mb-1 text-light">HISTORIAL</h3>
                         <hr class="text-light mx-auto rounded bg-white opacityOff mb-1">
                         <div class="tabla text-light mt-2">
