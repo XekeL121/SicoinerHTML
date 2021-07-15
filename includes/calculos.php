3c8428961eb4b5d84d5efd6f514edd7e5aecd95c
XekeL121 <oscarmahiques@gmail.com>
2021-07-15 23:15:07 +0200

    Maquetación cálculo colores

--- includes/calculos.php
+++ includes/calculos.php
@@ -35,24 +35,24 @@
                             </div>
                             <div class="calcCost text-light col-3">
                                 <h5>Cantidad</h5>
-                                <input class="rounded mt-1 mb-2 azul3 text-center text-light" type="text" value="200">
-                                <input class="rounded mb-2 text-center" type="text" value="10">
-                                <p>Cantidad total</p>
-                                <input class="rounded naranja azulLogo2 text-center" type="text"  value="210">
+                                <input class="rounded mt-1 mb-2 azul3 text-center w-75 cursor-na  verdeTrade opacityOff" type="submit" value="200">
+                                <input class="rounded mb-2 text-center w-75 cursor-pointer" type="text" value="10">
+                                <p class="mt-2">Cantidad total</p>
+                                <input class="rounded naranja azulLogo2 text-center w-75 cursor-na" type="submit"  value="210">
                             </div>
                             <div class="calcCost text-light col-3">
                                 <h5>Precio Compra</h5>
-                                <input class="rounded mt-1 mb-2 azul3 text-center text-light" type="text"  value="500 $">
-                                <input class="rounded mb-2 text-center" type="text" value="1,000 $">
-                                <p>Cost average</p>
-                                <input class="rounded naranja azulLogo2 text-center" type="text"  value="523.80 $">
+                                <input class="rounded mt-1 mb-2 azul3 text-center w-75 cursor-na verdeTrade opacityOff" type="submit"  value="500 $">
+                                <input class="rounded mb-2 text-center w-75 cursor-pointer" type="text" value="1,000 $">
+                                <p class="mt-2">Cost average</p>
+                                <input class="rounded naranja azulLogo2 text-center w-75 cursor-na" type="submit"  value="523.80 $">
                             </div>
                             <div class="calcCost text-light col-3">
                                 <h5>Inversión</h5>
-                                <input class="rounded mt-1 mb-2 azul3 text-center text-light" type="text"  value="100,000 $">
-                                <input class="rounded mb-2 text-center" type="text" value="10,000 $">
-                                <p>Total inversión</p>
-                                <input class="rounded naranja azulLogo2 text-center" type="text"  value="110,000 $">
+                                <input class="rounded mt-1 mb-2 azul3 text-center w-75 cursor-na  verdeTrade opacityOff" type="submit"  value="100,000 $">
+                                <input class="rounded text-light azulLogo2 mb-2 text-center w-75 cursor-na" type="submit" value="10,000 $">
+                                <p class="mt-2">Total inversión</p>
+                                <input class="rounded naranja azulLogo2 text-center w-75 cursor-na" type="submit"  value="110,000 $">
                             </div>
                         </div>
 
@@ -65,17 +65,17 @@
                             <div class="calcCost text-light col-3">
                                 <h5>Cantidad</h5>
                                 
-                                <input class="rounded mb-3 mt-2 text-center" type="text" value="100">
+                                <input class="rounded mb-3 mt-2 text-center cursor-pointer" type="text" value="100">
                             </div>
                             <div class="calcCost text-light col-3">
                                 <h5>Precio Compra</h5>
                                 
-                                <input class="azul3 text-light rounded mb-3 mt-2 text-center" type="text" value="1,000 $">
+                                <input class="azulLogo2 text-light rounded mb-3 mt-2 text-center cursor-pointer" type="text" value="1,000 $">
                             </div>
                             <div class="calcCost text-light col-3">
                                 <h5>Inversión</h5>
                                 
-                                <input class="rounded mb-3 mt-2 text-center naranja azulLogo2" type="text" value="100,000 $">
+                                <input class="rounded mb-3 mt-2 text-center text-light azulLogo2 cursor-pointer" type="text" value="100,000 $">
                             </div>
                         </div>
 
@@ -87,17 +87,17 @@
                             <div class="calcCost text-light col-3">
                                 <h5>Cantidad</h5>
                                 
-                                <input class="rounded mb-3 mt-2 text-center" type="text" value="100">
+                                <input class="rounded mb-3 mt-2 text-center cursor-pointer" type="text" value="100">
                             </div>
                             <div class="calcCost text-light col-3">
                                 <h5>Precio Compra</h5>
                                 
-                                <input class="azul3 text-light rounded mb-3 mt-2 text-center" type="text" value="1,000 $">
+                                <input class="azul3 text-light rounded mb-3 mt-2 text-center cursor-pointer" type="text" value="1,000 $">
                             </div>
                             <div class="calcCost text-light col-3">
                                 <h5>Inversión</h5>
                                 
-                                <input class="rounded mb-3 mt-2 text-center naranja azulLogo2" type="text" value="100,000 $">
+                                <input class="rounded mb-3 mt-2 text-center naranja azulLogo2 cursor-pointer" type="text" value="100,000 $">
                             </div>
                         </div>
                         
--- includes/historial.php
+++ includes/historial.php
@@ -1,8 +1,8 @@
                 <div class="ofertas p-2 px-5 row text-center justify-content-center overflow-scroll">
                     <!-- Aquí comienza el portfolio -->
                     <div class="tarjetas px-2 my-3 col col-lg-9 col-xl-8 col-md-8 col-8 text-center rounded">
-                        <h3 class="mt-3 mb-1 text-light">Historial</h3>
-                        <hr class="text-light mx-auto rounded bg-white opacityOff">
+                        <h3 class="mt-3 mb-1 text-light op80">Historial</h3>
+                        <hr class=" mx-auto rounded bg-white ">
                         <div class="tabla text-light">
                            <table class="table mt-1 mb-5 overflow-hidden">
                                 <thead>
