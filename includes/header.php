0a0931021362f81dbc6b1608367b8743f340877d
XekeL121 <oscarmahiques@gmail.com>
2021-07-15 22:02:58 +0200

    Maquetación header responsive

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,41 @@
             <header class="row justify-content-center azul1 py-2">
-                <div class="menuResponsive position-absolute text-center my-auto mt-4">
-                    <img class=" float-end col-2 text-center" src="img/reorder_white_24dp.svg" alt="">
-                </div>
                 <div class="divLogo col-xl-3 col-sm-8 col-12 p-2 text-center my-auto">
-                    <a href="index.php"> <img class="text-center" src="img/Isotipo Sicoiner v1.png" alt="logotipo Sicoiner"></a>
+                    <a href="login.php"> <img class="text-center" src="img/Isotipo Sicoiner v1.png" alt="logotipo Sicoiner"></a>
+                </div>
+
+                <!-- Menú responsive que aparece a partir de --> 
+                <div class="menuResponsive position-absolute flex-end">
+                    <ul class="d-flex row justify-content-center">
+
+                            <a href="index.php" class="navButon col-2 p-2 mt-2 text-light text-center">
+                                <li>
+                                    <img class="icon align-middle " src="img/app_registration_white_24dp.svg" alt="">                              
+                                </li>
+                            </a>
+
+                            <a href="historial.php" class="navButon col-2 p-2 mt-2 text-light text-center">
+                                <li>
+                                    <img class="icon" src="img/insights_white_24dp.svg" alt="">                                
+                                </li>
+                            </a>
+
+                            <div class="navButon col-2 p-2 mt-2 text-light text-center">
+                                
+                                <!-- Menú desplegable de botón usuario -->
+                                <button class="bg-transparent justify-content-right text-right">                                
+                                    <img class="icon" src="img/account_circle_white_24dp.svg" alt="">
+                                    <img class="icon" src="img/arrow_drop_down_white_18dp.svg" alt="">
+                                </button>
+                                <ul class="navAcount azulOscuro p-1 rounded">
+                                    <li class="px-3 rounded"><a class="h6 text-primary" href="">Guardar</a></li>
+                                    <li class="px-3 rounded"><a class="h6 text-light op80" href="">Mi cuenta</a></li>
+                                    <li class="px-3 rounded"><a class="h6 text-light op80" href="">Ayuda</a></li>
+                                    <li class="px-3 rounded"><a class="h6 text-light op80" href="">Versión Pro</a></li>
+                                    <li class="px-3 rounded"><a class="h6 text-light op80" href="">Acerca de..</a></li>
+                                    <li class="px-3 rounded"><a href="login.php" class="h6 rojoTrade">Cerrar sesión</a></li>
+                                </ul>
+                            </div>
+                        </ul>
                 </div>
                 <nav class="col-md-12 col-lg-12 col-xl-6 p-2 text-center">
                     <ul class="d-flex col-lg-12 justify-content-center">
--- includes/login.php
+++ includes/login.php
@@ -1,3 +1,4 @@
                 <div class="login text-center justify-content-center azul1 text-light align-items-center">
-                    Pag login
+                    <p>Página de login, falta maquetar</p>
+                    <a href="index.php">Inicio</a> 
                 </div>
