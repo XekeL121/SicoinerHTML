4d93ff3f04b4804f9177c7b161c21df510aee146
XekeL121 <oscarmahiques@gmail.com>
2021-07-15 02:09:03 +0200

    cacarrrr

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,4 @@
-<footer class="text-center text-light my-auto d-flex justify-content-center p-5 azulOscuro naranja">
-    Sicoiner | 2021
+<footer class="text-center text-light my-auto justify-content-center p-5 azulOscuro naranja">
+  <p>Sicoiner | 2021</p>   
+  <p class="text-light op50"> v1.0 Pro</p>
 </footer>
