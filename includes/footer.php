218917e55f43420f9ae37ab7431c6053dca123f2
XekeL121 <oscarmahiques@gmail.com>
2021-07-28 12:44:09 +0200

    retoque footer, se quedaba flotando

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
-<footer class="mb-0 text-center text-light justify-content-center p-5 azulOscuro text-orange">
+<footer class="d-end text-center text-light justify-content-center p-5 azulOscuro text-orange">
   <p>Sicoiner | 2021</p>   
   <p class="text-light op50"> v1.0 Pro</p>
 </footer>
