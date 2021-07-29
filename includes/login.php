8cba843bc8f3ed8b574335dab89f881453b1e393
XekeL121 <oscarmahiques@gmail.com>
2021-07-30 00:26:48 +0200

    página login 50%

--- includes/login.php
+++ includes/login.php
@@ -1,7 +1,63 @@
-            <div class="container-full row azulFondo"> 
-                <div class="login text-center justify-content-center azul1 text-light align-items-center">
-                    <p>Página de login, falta maquetar</p>
-                    <a href="index.php">Inicio</a> 
-                </div>               
+            
+            <div id="cont-log" class="d-flex row justify-content-center text-center align-items-start">
+                
+                    <div id="logo-log" class="col-12">
+                        <img class="mt-5" src="img/Isotipo Sicoiner v1.png" alt="logotipo Sicoiner">
+                        
+                    </div>
+                    
 
+
+
+
+                    <div id="formu-log" class="op80 azul1 d-flex col-12 col-md-8 col-xl-5 rounded row align-items-center pb-5 pt-4 nowrap">
+                       <!--  <div class=""> -->
+                            <div id="form-social">
+                                <div id="icon-log">
+                                    <img  class="azulFondo rounded-circle mb-2 p-2" src="img/person_white_48dp.svg" alt="">
+                                </div>
+                                <h3 class="text-uppercase">Registro de usuario</h3>
+                                <div class="my-3 w-100 mx-auto">
+                                    <label class="mb-1">Email</label>
+                                    <div class="mx-auto my-auto d-flex justify-content-center">
+                                        <input type="text" class="mx-auto my-auto px-2 py-1 text-center azulLogo2 rounded text-light border-none" id="inputEmail" value="email@example.com">
+                                    </div>
+                                </div>
+
+                                <div class="mb-3 w-100 mx-auto my-auto">
+                                    <label class="mb-1">Password</label>
+                                    <div class="mx-auto my-auto d-flex justify-content-center">
+                                    <input type="password" class="mx-auto my-auto px-2 py-1 text-center azulLogo2 rounded text-light border-none" id="inputPassword">
+                                    </div>
+                                </div>
+
+                                <div class="d-flex row">
+                                    <div class="col-6">Recordarme</div>
+                                    <div class="col-6">Recuperar contraseña</div>
+                                </div>
+                                <h6>Inciar sesión con:</h6>
+                                <div class="d-flex row justify-content-center">
+                                    <div class="col-2">Google</div>
+                                    <div class="col-2">Facebook</div>
+                                    <div class="col-2">Twitter</div>
+                                </div>                            
+                            </div>    
+                        
+                            <div class="d-flex row">
+                                <a href="index.php"><button type="button" class="py-2 mt-5 bg-orange rounded text-dark text-bold">Acceder</button></a>
+                            </div>
+                       <!--  </div> -->
+
+                        
+                        
+                        
+                    </div>               
+
+
+
+
+
+
+
+                
             </div>
